d19cf5fea8972c04ef48d4de2c4f632de60d1449
dani7beth <danielledona28@gmail.com>
2020-12-17 12:05:58 -0700

    added media query

--- src/Github.js
+++ src/Github.js
@@ -1,7 +1,8 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 import {useState, useEffect} from 'react';
 import Axios from 'axios';
-import { Card, Grid } from 'semantic-ui-react';
+import { Card, Grid, Icon } from 'semantic-ui-react';
+import {device} from './styled/sizes'
 
 export default () =>{
 
@@ -19,27 +20,99 @@ export default () =>{
     getRepos();
   },[])
 
-  return(
-    <>
+  return (
     <Grid>
       <Grid.Row>
-        {repos.map((r)=> (
-          <Grid.Column key={r.id} width={4}>
-            <Card>
-              <Card.Content>
-                <Card.Header>
-                  {r.full_name}
-                </Card.Header>
-                <Card.Meta>
-                  {r.description}
-                </Card.Meta>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-        ))}
+        {repos &&
+          repos.map((r) => (
+            <Grid.Column key={r.id} width={4}>
+              <StyledCard color="red">
+                <Card.Content>
+                  <Card.Header>
+                    <Truncated>
+                      <Star>
+                        <Icon name="star" />
+                      </Star>
+                      {r.full_name}
+                    </Truncated>
+                  </Card.Header>
+                  <Card.Meta>
+                    <Truncated wrap>{r.description}</Truncated>
+                  </Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                  <ButtonLink href={r.html_url} target="_blank">
+                    view
+                  </ButtonLink>
+                </Card.Content>
+              </StyledCard>
+            </Grid.Column>
+          ))}
       </Grid.Row>
     </Grid>
-      
-    </>
-  )
-}
+  );
+};
+
+const ButtonLink = styled.a`
+  float: right;
+  padding: 10px 30px;
+  color: white !important;
+  background-color: black;
+`;
+
+const StyledButton = styled(ButtonLink)`
+  display: flex;
+  background: #234567;
+  color: white;
+  padding: 15px 30px !important;
+  justify-content: center;
+  transition: background .2s ease;
+  cursor: pointer;
+
+  &:hover{
+    background: #765432;
+    transition: background .2s ease;
+  }
+`;
+
+
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  } 
+  to{
+    transform: rotate(360deg);
+  }
+ `;
+
+const StyledCard = styled(Card)`
+  height: 200px !important;
+  margin-bottom: 10px !important;
+  @media ${device.laptop}{
+    min-height: 400px !important;
+    background-color: blue !important;
+  }
+  @media ${device.tablet}{
+    min-height: 400px !important;
+    background-color: red !important;
+  }
+  @media ${device.mobile}{
+    min-height: 400px !important;
+    background-color: green !important;
+  }
+`;
+
+const Truncated = styled.div`
+  white-space: ${(props)=> props.wrap ? 'wrap' : 'nowrap'};
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+`;
+
+const Star = styled.div`
+  display: inline-block;
+  color: yellow;
+  text-shadow: 1px 2px 1px black;
+  // animation: ${rotate360} 2s linear infinite;
+`;
